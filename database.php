bf1b11e995d1b311f96056969cfe75bb11ca3773
Kirny <kirnyx3@gmail.com>
2016-12-01 01:08:56 -0500

    Added login.php and database.php

---
+++ database.php
@@ -0,0 +1,7 @@
+// This should be stored outside of the public directly so it can't be accessed via HTTP. However server will need to be reconfigured.
+<?php
+$hn = 'classroom.cs.unc.edu';
+$db = 'kirnydb';
+$usr = 'kirny';
+$pw = 'Comprocks#123';
+?>
---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+
+session_start();
+
+include 'database.php';
+
+/* BUILD A CONNECTION WITH SQL */
+$conn = new mysqli($hn, $usr, $pw, $db);
+
+function check_password($username, $password) {
+
+  $sql = "SELECT username, password FROM Users WHERE username = '$username'";
+
+  $result = mysqli_query($conn, $sql);
+
+  if($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    if(md5($password) == md5($row["password"]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+$username = $_GET['username'];
+$password = $_GET['password'];
+
+if (check_password($username, $password)) {
+  header('Content-type: application/json');
+
+  // Generate authorization cookie
+  $_SESSION['username'] = $username;
+  $_SESSION['authsalt'] = time();
+
+  $auth_cookie_val = md5($_SESSION['username'] . $_SERVER['REMOTE_ADDR'] . $_SESSION['authsalt']);
+
+  setcookie('BANKAUTH', $auth_cookie_val, 0, '/Courses/comp426-f16/users/kirny/ProjectArea', 'wwwp.cs.unc.edu', true); //ProjectArea can be changed.
+
+  print(json_encode(true));
+
+} else {
+  unset($_SESSION['username']);
+  unset($_SESSION['authsalt']);
+
+  header('HTTP/1.1 401 Unauthorized');
+  header('Content-type: application/json');
+  print(json_encode(false));
+}
