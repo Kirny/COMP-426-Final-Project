644c04bd10491a504c207eae607479e0653cf653
Kirny <kirnyx3@gmail.com>
2016-12-01 22:07:27 -0500

    added a few more changes

--- login.js
+++ login.js
@@ -15,4 +15,20 @@ $(document).ready(function () {
         		  alert('Username or Password incorrect');}
 	        });
   });
+
+  $('.register-form').on('submit', function (e) {
+	e.stopPropagation();
+	e.preventDefault();
+	$.ajax('login.php',
+	       {type: 'GET',
+        	data: $('.login-form').serialize(),
+        	cache: false,
+        	success: function () {
+        		  alert('Login Successful');
+              window.location ='welcomepage.html';
+          },
+        	error: function () {
+        		  alert('Username or Password incorrect');}
+	        });
+  });
 });
