f74a30f9bab7e7b126fc0be8b8f3e857f072c8ca
Daniel Lee <DanielLee@dhcp-v880-9-00760.1Xwireless.unc.edu>
2016-12-06 15:19:41 -0500

    before BF

--- public/transfer.js
+++ public/transfer.js
@@ -106,6 +106,7 @@ var selection = function() {
                                                         alert(jqXHR.responseText);
                                           }});
                                         }
+                                        //w--
                                         //location.href = "welcomepage.php";
                         }); //"Transfer Now"
           	         }//1st success
